e4ac4271f98c6b0b529e8d63be9897891555ff7e
pouyababaie <pouyababie123@gmail.com>
2023-05-13 17:29:19 +0430

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
+  app.enableCors({origin:'http://localhost:4200'});
   await app.listen(3000);
 }
 bootstrap();
